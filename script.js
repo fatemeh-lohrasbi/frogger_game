a7bfa94772051060cc40cdaabb1c092f1b4f687c
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2022-10-20 17:31:47 +0330

    move log left function added

--- script.js
+++ script.js
@@ -4,24 +4,56 @@ const start_pause_btn = document.querySelector('#start_pause_btn')
 const squares = document.querySelectorAll('.grid div')
 let current_index = 76; //our starting block is on 76 index
 const width = 9;
+const log_left = document.querySelectorAll('.log_left')
 
-function move_frog(e){
+function move_frog(e) {
     squares[current_index].classList.remove('frog')
 
-    switch(e.key){
+    switch (e.key) {
         case 'ArrowLeft':
             if (current_index % width !== 0) current_index -= 1  // if frog is not on 0,9,18,27,36,45, ...             
-        break
+            break
         case 'ArrowRight':
-            if (current_index % width < width - 1) current_index += 1            
-        break
+            if (current_index % width < width - 1) current_index += 1
+            break
         case 'ArrowUp':
-            if (current_index - width >= 0) current_index -= width            
-        break
+            if (current_index - width >= 0) current_index -= width
+            break
         case 'ArrowDown':
-            if (current_index + width < width * width) current_index += width            
-        break
+            if (current_index + width < width * width) current_index += width
+            break
     }
     squares[current_index].classList.add('frog')
 }
 document.addEventListener('keydown', move_frog)
+
+function auto_move_logs() {
+    log_left.forEach(element => move_log_left(element));
+}
+
+function move_log_left(log_left) {
+    switch (true) {
+        case log_left.classList.contains('l1'):
+            log_left.classList.remove('l1')
+            log_left.classList.add('l2')
+            break
+        case log_left.classList.contains('l2'):
+            log_left.classList.remove('l2')
+            log_left.classList.add('l3')
+            break
+        case log_left.classList.contains('l3'):
+            log_left.classList.remove('l3')
+            log_left.classList.add('l4')
+            break
+        case log_left.classList.contains('l4'):
+            log_left.classList.remove('l4')
+            log_left.classList.add('l5')
+            break
+        case log_left.classList.contains('l5'):
+            log_left.classList.remove('l5')
+            log_left.classList.add('l1')
+            break
+    }
+}
+
+setInterval(auto_move_logs, 1000)
